af77f8e500587f9b12d43c9742303cd2d20c24bb
Kaven arango <43826797+KavenArango@users.noreply.github.com>
2020-09-23 21:07:32 -0400

    changes

--- Regretion.py
+++ Regretion.py
@@ -1,4 +1,3 @@
-from __future__ import division
 import pandas as pd
 import math
 from decimal import Decimal
@@ -86,26 +85,18 @@ def get_sum_residuals(list1, list2):
 
 def do_all_regression_stuff(x, y):
     mean_exploratory = mean(x)
-    print('Mean of the exploratory (independent) variable = ', mean_exploratory)
     mean_response = mean(y)
-    print('Mean of response (dependent) variable = ', mean_response)
     
     cov_xy = round(covariance(x, y), 4)
-    print('Covariance between exploratory and response variables = ', cov_xy)
     var_x = round(variance(x), 4)
-    print('Variance of exploratory (independent) variable = ', var_x)
     
     beta_1_hat = round((cov_xy/var_x), 6) # Slope of the best fit straight line
-    print('beta_1_hat (slope of the best fit straight line) = ', beta_1_hat)
     beta_0_hat = round((mean_response - beta_1_hat * mean_exploratory), 6) # Intercept of the best fit straight line
-    print('beta_0_hat (intercept of the best fit straight line) = ', beta_0_hat)
-    
     response_value_list = []
+    
     for x_i in x:
         response_value_list.append(round(get_y_pred(x_i, beta_1_hat, beta_0_hat), 2))
-        # print('Response Value List = ', response_value_list)
-        # print('------------------')
-    print('Response value list is ', response_value_list)
+    
     plt.scatter(x, y, color='red', marker='o', s=30)
     plt.plot(x, response_value_list, color = "g")
     plt.xlabel('Exploratory (independent) variable values') 
@@ -113,21 +104,13 @@ def do_all_regression_stuff(x, y):
     plt.show()
     
     errors = get_residuals(y,response_value_list)
-    print('Errors or Residuals of Predicted Response value and Actual Exploratory value =', errors)
     
     response_sum_residuals = get_sum_residuals (y, response_value_list)
-    print('Sum of Residuals of Predicted and Actual Response Values = ', response_sum_residuals)
-
-    # print('===================')
     response_value_ssr = round(get_ssr(y, response_value_list), 4)
-    print('Sum Squared Residuals of Predicted and Actual Response Values = ', response_value_ssr)
+    
     
     r_xy = correlation(x, y)
-    print('Correlation between Exploratory and Response variables = ', round(r_xy, 4))
-
     r_sq = r_xy**2 * 100
-    print('R-square between Exploratory and Response variables = ', round(r_sq, 4))
-    print('Goodness of fit :', round(r_sq, 4) , '% of the Response Variable values are explained by the line of best fit')
     
     newdf = pd.DataFrame({
         'X Mean':[mean_exploratory],
@@ -144,15 +127,3 @@ def do_all_regression_stuff(x, y):
     newdf = newdf[['X Mean', 'Y Mean', 'Beta-0(Slope)', 'Beta-1(Intercept)', 'SumOfResiduals', 'SumSquareResiduals', 'R-XY(Correlation)', 'R-Square-XY', 'Goodness of fit(%)']]
     
     return newdf
-
-
-NewCases = pd.read_csv("sevenday_rolling_average_of_new_cases.csv").head(100) # converts .csv to dataframe
-NewDeaths = pd.read_csv("sevenday_rolling_average_of_new_deaths.csv").head(100) # converts .csv to dataframe
-
-# ca fl ga il mi tx
-
-
-do_all_regression_stuff(NewCases['GA'], NewDeaths['GA'])
-
-print(NewCases)
-
